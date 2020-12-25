7f9fdc59185b7f0123a4775702c20cda3da85001
Poosana  Thassanavisut <poosanathassanavisut@Poosanas-MacBook-Pro.local>
2020-12-25 14:59:59 +0700

    3 done

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	char y = int(x)-1;
+	if(int(x) > 90 || int(x) < 65){
+		return '0';
+	}else{
+	if ((x)-1 == 64){
+			y = 'Z';
+		}
+		return y;
+	}
 }
 
 int main(){
